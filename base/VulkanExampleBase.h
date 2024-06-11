3fad48c6cf54664e4a84df5c09d60e77bfe0ef22
rgosens2 <39185953+rgosens2@users.noreply.github.com>
2024-06-12 00:33:38 +0200

    cleanup VulkanExampleBase.h

--- base/VulkanExampleBase.h
+++ base/VulkanExampleBase.h
@@ -179,7 +179,6 @@ public:
 	xcb_window_t window;
 	xcb_intern_atom_reply_t *atom_wm_delete_window;
 #elif defined(VK_USE_PLATFORM_MACOS_MVK)
-	// RG:
 	std::string gltfFileName = "";
 	bool ingltfFileButton = false;
 	bool opengltfFileButtonClicked = false;
@@ -243,9 +242,7 @@ public:
 	void initxcbConnection();
 	void handleEvent(const xcb_generic_event_t *event);
 #elif defined(VK_USE_PLATFORM_MACOS_MVK)
-	// RG:
 	void showOpenFileDialog();
-	//NSAlert* showMessageBox();
 	NSModalResponse showMessageBox();
 	NSWindow* setupWindow();
 	void mouseDragged(float x, float y);
